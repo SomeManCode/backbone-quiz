e83af047805243488c50def2a62396b67b8f154c
Vanga Sasidhar <sasidhar@sasidhar.com>
2012-12-01 14:49:36 +0530

    Added Quiz View Template.

--- app/views/QuizView.js
+++ app/views/QuizView.js
@@ -1,19 +1,23 @@
 /*Quiz View*/
 
 define(
-	['backbone'],
-	function(Backbone){
+	[
+        'underscore',
+        'backbone',
+        'text!templates/QuizViewTemplate.html'
+    ],
+	function(_, Backbone, QuizViewTemplate){
 		var QuizView = Backbone.View.extend({
 			id:"quiz_view",
-			
 			className:"section",
+            template : QuizViewTemplate,
 			
 			initialize:function(){
 				
 			},
 			
 			render:function(){
-				$(this.el).html("Quiz View");
+				$(this.el).html(_.template(QuizViewTemplate));
 				return this;
 			}
 		})
