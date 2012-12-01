f506ee31850a855f2e3a7abd9755721498b65e1c
mohanrajt <mohanraj.t@imaginea.com>
2012-12-01 14:51:01 +0530

    update score view and added quiz.css

--- app/views/ScoreView.js
+++ app/views/ScoreView.js
@@ -1,16 +1,20 @@
 /* Score View */
 define(
-    ['backbone'],
-    function (Backbone) {
+    [
+		'backbone',
+		'text!templates/ScoreViewTemplate.html'
+	],
+    function (Backbone, ScoreViewTemplate) {
     
         var ScoreView = Backbone.View.extend({
             id : "score_view",
             className : "section",
+			template : ScoreViewTemplate,
             initialize : function() {
                 
             },
             render : function() {
-                $(this.el).html("Score");
+                $(this.el).html(_.template(this.template));
                 return this;
             }
         });
